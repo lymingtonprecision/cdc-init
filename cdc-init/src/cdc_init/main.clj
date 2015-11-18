6cdc0a9af2b8b2b06d5a0179bd75db124d161760
Adam Harper <github@adam-harper.com>
2015-11-18 09:42:33 +0000

    Move logging initialization to a fn
    To avoid overwriting any existing initialization if the file is
    loaded in the REPL.

--- cdc-init/src/cdc_init/main.clj
+++ cdc-init/src/cdc_init/main.clj
@@ -10,34 +10,36 @@
 
 (def timestamped-log-pattern "%d %-5p [%c] %m%n")
 
-(set-loggers!
- :root
- {:pattern timestamped-log-pattern
-  :level :info}
+(defn init-logging! []
+  (set-loggers!
+   :root
+   {:pattern timestamped-log-pattern
+    :level :info}
 
- ["kafka"
-  "org.apache.kafka"
-  "org.apache.zookeeper"
-  "org.I0Itec.zkclient"]
- {:name "Kafka"
-  :level :error}
+   ["kafka"
+    "org.apache.kafka"
+    "org.apache.zookeeper"
+    "org.I0Itec.zkclient"]
+   {:name "Kafka"
+    :level :error}
 
- "org.apache.zookeeper.ZooKeeper"
- {:name "ZooKeeper"
-  :level :error}
+   "org.apache.zookeeper.ZooKeeper"
+   {:name "ZooKeeper"
+    :level :error}
 
- "com.zaxxer.hikari"
- {:name "HikariCP"
-  :level :error})
+   "com.zaxxer.hikari"
+   {:name "HikariCP"
+    :level :error})
 
-(Thread/setDefaultUncaughtExceptionHandler
- (reify Thread$UncaughtExceptionHandler
-   (uncaughtException [_ thread ex]
-     (log/error ex "Uncaught exception on" (.getName thread)))))
+  (Thread/setDefaultUncaughtExceptionHandler
+   (reify Thread$UncaughtExceptionHandler
+     (uncaughtException [_ thread ex]
+       (log/error ex "Uncaught exception on" (.getName thread))))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Main
 
 (defn -main [& args]
+  (init-logging!)
   (let [sys (component/start (new-system))]
     (.addShutdownHook (Runtime/getRuntime) (Thread. #(component/stop sys)))))
